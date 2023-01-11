c74806a21c214461e18526e3e184d3db7ec6bebf
ByronEricPerez <xxloboisthemanxx@gmail.com>
2023-01-11 19:51:38 -0300

    add, mark or unmark elements of the 'todo' done

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 
 const Input = () => {
 
-    const [items, setItems] = useState([]);
-    const [name, setName] = useState('')
+    const [items, setItems] = useState([]);//guarda un array de los elemntos agregados en el input
+    const [name, setName] = useState('')//guarda una cadena vacía que almacena el valor del input
 
-    const addElement = () => {
+    const addElement = () => {//agrega un nuevo objeto al estado items con el nombre del input del usuario.
         const newItem = {name: name};
         setItems([...items, newItem]);
     };
@@ -16,21 +16,32 @@ const Input = () => {
     window.onkeyup = keyup;
 
 
-    function keyup(e) {
+    function keyup(e) {//cuando se ejecuta esta función se actualiza el estado name con el valor del input
         setName(e.target.value);
         if (e.keyCode === 13) {
-            console.log(items);
             addElement();
         }
     }
-
     
     return (
         <h2>
-            <input type="text"  placeholder="Que quieres anotar?"/>
+            <input type="text" placeholder="Que quieres anotar?"/>
             <ul className="flex flex-col mt-5 text-center">
-                <TodoList item={items}/>
-                {items.map((item, index) => (<li key={index}>{item.name}</li>))}
+                {/* <TodoList /> */}
+                {items.map((item, index) => (//itera sobre el estado items, que es un arreglo de objetos que contiene el nombre y el estado de tachado para cada elemento.
+                    <li //se crea un li para cada elemento del estado items.
+                    onClick={() => {//
+                        const newItems = [...items];//Se crea una copia del estado actual de los items,
+                        newItems[index].isUnderlined = !newItems[index].isUnderlined;//, se cambia el estado tachado del item actual, 
+                        setItems(newItems);//y luego se actualiza el estado con la copia de los items actualizada para que el estado tachado del elemento se invierta.
+                    }} 
+                    style={{textDecoration: item.isUnderlined //establece en función del estado del item, si es true se tacha el texto si no se remueve el tachado.
+                        ? 'line-through'
+                        : 'none'}} //operador ternario
+                    >
+                    {item.name}
+                    </li>
+                ))}
             </ul>
         </h2>
     )
--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,11 +1,17 @@
 
 const TodoList = ({items}) => {
     return (
-        <ul className="flex flex-col mt-5 text-center">
-            {/* {items.map((item, index) => (
-            <li key={index}>{item.name}</li>))} */}
-        </ul>
+      <ul className="flex flex-col mt-5 text-center">
+        {/* {items.map((item, index) => (
+          <li key={index}>
+            <div className="input-wrapper">
+              <input type="checkbox" id={`checkbox-${index}`} />
+              <label htmlFor={`checkbox-${index}`}>{item.name}</label>
+            </div>
+          </li>
+        ))} */}
+      </ul>
     );
-}
-
-export { TodoList };
+  }
+  
+  export { TodoList };
